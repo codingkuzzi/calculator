6a5febf70150781596ff478fc51159c370f69557
Anna Kuznetsova and Saul Artiga <anna+saul@>
2017-06-01 15:42:22 -0700

    add radio buttons,complete js functions, add styles

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,21 @@ var divide = function(number1, number2) {
 };
  //Jquery front end below
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").empty().append(result);
   });
 });
